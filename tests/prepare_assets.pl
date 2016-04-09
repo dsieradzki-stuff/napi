a0e9034b9292fe97fd215b6648824866ddad9a77
Tomasz Wisniewski <tomasz.wisni3wski@gmail.com>
2016-04-09 11:36:17 +0100

    Correct prepare_assets

--- tests/prepare_assets.pl
+++ tests/prepare_assets.pl
@@ -10,25 +10,51 @@ use Archive::Extract;
 use File::Temp;
 
 
-my $assets_tgz = "napi_testdata.tgz";
-my $url = "https://www.dropbox.com/s/xz3pfvkj5d8zp6i/${assets_tgz}?dl=0";
+my $assets_tgz = "napi_testdata.tar.gz";
+my $url = "https://www.dropbox.com/s/x0xzw0b71j0dtop/${assets_tgz}?dl=1";
 my $assets_path = "/usr/share/napi";
 
 
 my $wdir = File::Temp::tempdir( CLEANUP => 1 );
 
 
-print "Downloading $assets_tgz ";
-my $status = getstore( $url, $wdir . '/' . $assets_path );
-
-print 200 == $status ? "OK" : "FAIL";
-print "\n";
-
-if (200 == $status) {
-    die "Unable to create the architecture independent data directory\n"
-        unless ( -e $assets_path || mkdir ($assets_path) );
-
-    my $ae = Archive::Extract->new(
-        archive => $wdir . '/' . $assets_tgz );
-    $ae->extract( to => $assets_path ) and print "Unpacked assets\n";
+sub print_status {
+    my ($msg, $coderef, $expected) = @_;
+    print $msg . " ... ";
+    my $retval = &{$coderef}();
+    print $retval == $expected ? "OK" : "FAIL";
+    print "\n";
+    return $retval;
 }
+
+
+sub on_success {
+    my ($retval, $coderef, $expected) = @_;
+    $coderef->() if $retval == $expected;
+}
+
+
+on_success(
+    print_status(
+        "Downloading $assets_tgz",
+        sub {
+            getstore( $url, $wdir . '/' . $assets_tgz );
+        },
+        200),
+    sub {
+        die "Unable to create the architecture independent data directory\n"
+            unless ( -e $assets_path || mkdir ($assets_path) );
+
+        print_status(
+            "Unpacking assets",
+            sub {
+                my $archive = $wdir . '/' . $assets_tgz;
+                my $ae = Archive::Extract->new(
+                    archive => $archive );
+                $ae->extract( to => $assets_path )
+            },
+            1
+        );
+    },
+    200
+);
